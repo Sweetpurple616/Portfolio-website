7e9ea2551bb32de62168ae3eed807baf5578f243
Sweetpurple616 <zoedewaard@hotmail.nl>
2019-05-15 11:23:45 +0200

    added pictures and links

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container grid-list-xl class="first">
     <v-layout v-bind="binding">
-<v-flex xs12 first>
+      <v-flex xs12 first>
             <v-card color="white" class="black--text">
               <v-layout row>
                 <v-flex xs5>
@@ -23,6 +23,8 @@
                   </v-card-title>
                   <v-divider></v-divider>
                   <v-card-text>
+                    Ik heb mijn portfolio website gemaakt met VueJS, als eerste had ik een met HTML en CSS gemaakt. Hier was ik niet tevreden over dus heb ik gekozen om een nieuwe portfolio te maken met VueJS. Ik had hiervoor al een beetje ervaring met VueJS maar ik wilde meer over VueJS leren. 
+                    Tijdens het maken van deze website heb ik vooral geleerd over hoe ik componenten gebruik en hoe ik een website kan stylen met Vuetify.
                   </v-card-text>
                 
                 </v-flex>
@@ -38,21 +40,26 @@
                 </v-flex>
                 <v-flex xs2 align-self-center
                 >
-                  <v-card-text>
-                    <v-icon large color="red">fa-youtube</v-icon>
+                  <v-card-text> <a href="https://www.zoedewaard.nl/">
+                    <v-icon large color="black">fa-mouse-pointer</v-icon> </a>
                   </v-card-text>
                 </v-flex>
                 <v-flex xs1>
                   <v-divider vertical></v-divider>
                 </v-flex>
-                <v-flex xs7 align-content-space-around
->
-                  <v-flex xs1>
-                  <v-car-text>
-                  <v-icon large>fa-github</v-icon>
-                  </v-car-text>
-                  </v-flex>
+                                               <v-flex xs7 align-content-space-around>
+                                 <v-layout row>
+
+                  <v-flex xs4>
+               
+                   <h3>
+                  Vue</h3>
+                               
+                   </v-flex>
+      
+                                 </v-layout>
                 </v-flex>
+ 
               </v-layout>
             </v-card>
           </v-flex>
@@ -79,7 +86,7 @@
                   </v-card-title>
                   <v-divider></v-divider>
                   <v-card-text>
-                    In dit project heb ik als eerste een API gemaakt met de node, express en mongoDB. Daarna heb ik een Front-End gemaakt met Vuejs
+                    Dit is een Android App die ik heb gemaakt. De App laat een evenement zien die er als eerste aankomt in een stad. Dit doe ik met de hulp van de API van Ticketmaster. De stad kan in de settings worden gekozen en wordt hier opgeslagen. Dit was de eerste keer dat ik een mobile App heb gemaakt. Dit project was vooral het kennis maken met Android studio en het leren van Kotlin.
                     
 
                   </v-card-text>
@@ -96,15 +103,29 @@
                   </v-card-text>
                 </v-flex>
                 <v-flex xs2>
-                  <v-card-text>
-                    <v-icon large color="red">fa-youtube</v-icon>
+                  <v-card-text> <a href="https://youtu.be/UzFxFO7Izmo">
+                    <v-icon large color="red">fa-youtube</v-icon> </a>
                   </v-card-text>
                 </v-flex>
                 <v-flex xs1>
                   <v-divider vertical></v-divider>
                 </v-flex>
-                <v-flex xs7>
-                  lang
+                               <v-flex xs7 align-content-space-around>
+                                 <v-layout row>
+
+                  <v-flex xs4>
+               
+                   <h3>
+                  Android Studio </h3>
+                               
+                   </v-flex>
+      
+                  <v-flex xs4>
+                   <h3>
+                  Kotlin </h3>              
+                
+                   </v-flex>
+                                 </v-layout>
                 </v-flex>
               </v-layout>
             </v-card>
@@ -131,8 +152,9 @@
                     </div>
                   </v-card-title>
                   <v-divider></v-divider>
-                  <v-card-text>
-                    In dit project heb ik als eerste een API gemaakt met de node, express en mongoDB. Daarna heb ik een Front-End gemaakt met Vuejs
+                   <v-card-text>
+                    In dit project heb ik een REST webapplicatie gemaakt waarin je projecten kan aanmaken, bewerken en verwijderen. Als eerste heb ik een API gemaakt die volledig CRUD is met ExpressJS, MongoDB en NodeJS op een Ubuntu server.
+                    Daarna heb ik een front-end gemaakt in VueJS met NodeJS. Dit project was de eerste keer dat ik met ExpressJS en VueJS werkte. 
                     
 
                   </v-card-text>
@@ -148,15 +170,42 @@
                   </v-card-text>
                 </v-flex>
                 <v-flex xs2>
-                  <v-card-text>
-                    <v-icon large color="red">fa-youtube</v-icon>
+                  <v-card-text> <a href="https://github.com/Sweetpurple616/ProjectFullstack">
+                    <v-icon large color="red">fa-youtube</v-icon> </a>
                   </v-card-text>
                 </v-flex>
                 <v-flex xs1>
                   <v-divider vertical></v-divider>
                 </v-flex>
-                <v-flex xs7>
-                  lang
+                                               <v-flex xs7 align-content-space-around>
+                                 <v-layout row>
+
+                  <v-flex xs4>
+               
+                   <h3>
+                  MongoDB</h3>
+                               
+                   </v-flex>
+                   <v-flex xs4>
+               
+                   <h3>
+                  ExpressJS</h3>
+                               
+                   </v-flex>
+                   <v-flex xs4>
+               
+                   <h3>
+                  VueJS</h3>
+                               
+                   </v-flex>
+                   <v-flex xs4>
+               
+                   <h3>
+                  NodeJS</h3>
+                               
+                   </v-flex>
+      
+                                 </v-layout>
                 </v-flex>
               </v-layout>
             </v-card>
@@ -168,7 +217,7 @@
                 <v-flex xs5>
 
                   <v-img
-                    :src="require('@/assets/project/MusicNowPlease.jpg')"
+                    :src="require('@/assets/project/sails.jpg')"
                     height="400"
                     contain
                     
@@ -177,14 +226,14 @@
                 <v-flex xs7>
                   <v-card-title primary-title>
                     <div>
-                      <div class="headline">Sailsjs</div>
+                      <div class="headline">SailsJS</div>
                       <div>MVC Framework</div>
                       <div>11-2018</div>
                     </div>
                   </v-card-title>
                   <v-divider></v-divider>
                   <v-card-text>
-                    In dit project heb ik als eerste een API gemaakt met de node, express en mongoDB. Daarna heb ik een Front-End gemaakt met Vuejs
+                  In dit project heb ik een CRUD web applicatie gemaakt een MVC framework, hier heb ik SailsJS voor gebruikt. Op de web applicatie kan worden ingelogd. Je kan een foto uploaden en deze kunnen andere mensen raten. Met dit project heb ik vooral veel geleerd over mijzelf. Het framework was best wel lastig en het ging allemaal niet zo lekker. Ik heb geleerd dat ik enorm goed kan doorzetten. En logisch kan nadenken op momenten dat het niet zo goed lukt. Uiteindelijk is het niet helemaal gelukt om hem werkend te krijgen. Maar dit was minder belangrijk dat het leerproces. 
                     
 
                   </v-card-text>
@@ -200,15 +249,28 @@
                   </v-card-text>
                 </v-flex>
                 <v-flex xs2>
-                  <v-card-text>
-                    <v-icon large color="red">fa-youtube</v-icon>
-                  </v-card-text>
+
                 </v-flex>
                 <v-flex xs1>
                   <v-divider vertical></v-divider>
                 </v-flex>
-                <v-flex xs7>
-                  lang
+                                              <v-flex xs7 align-content-space-around>
+                                 <v-layout row>
+
+                  <v-flex xs4>
+               
+                   <h3>
+                  SailsJS</h3>
+                               
+                   </v-flex>
+                                     <v-flex xs4>
+               
+                   <h3>
+                  MySQL</h3>
+                               
+                   </v-flex>
+      
+                                 </v-layout>
                 </v-flex>
               </v-layout>
             </v-card>
@@ -219,7 +281,7 @@
                 <v-flex xs5>
 
                   <v-img
-                    :src="require('@/assets/project/MusicNowPlease.jpg')"
+                    :src="require('@/assets/project/game1.jpg')"
                     height="400"
                     contain
                     
@@ -228,15 +290,14 @@
                 <v-flex xs7>
                   <v-card-title primary-title>
                     <div>
-                      <div class="headline">CLE GAME</div>
+                      <div class="headline">Ape of Divinity</div>
                       <div>Typescript/phaser game</div>
                       <div>06-2018</div>
                     </div>
                   </v-card-title>
                   <v-divider></v-divider>
                   <v-card-text>
-                    In dit project heb ik als eerste een API gemaakt met de node, express en mongoDB. Daarna heb ik een Front-End gemaakt met Vuejs
-                    
+Dit is een groepsproject geweest, in dit project hebben we een leerzame game gemaakt voor kinderen. In onze game bestuur je een aapje die een avontuur beleeft terwijl hij de Griekse goden leert kennen. In dit project hebben we met PhaserJS en typescript gewerkt. In dit project was mijn taak vooral het programmeren. Ik heb verschillende delen geprogrammeerd zoals de functionaliteit van de speler tot aan het plaatsen van level elementen.                    
 
                   </v-card-text>
                 
@@ -251,15 +312,28 @@
                   </v-card-text>
                 </v-flex>
                 <v-flex xs2>
-                  <v-card-text>
-                    <v-icon large color="red">fa-youtube</v-icon>
-                  </v-card-text>
+
                 </v-flex>
                 <v-flex xs1>
                   <v-divider vertical></v-divider>
                 </v-flex>
-                <v-flex xs7>
-                  lang
+                                              <v-flex xs7 align-content-space-around>
+                                 <v-layout row>
+
+                  <v-flex xs4>
+               
+                   <h3>
+                  Typescript</h3>
+                               
+                   </v-flex>
+                     <v-flex xs4>
+               
+                   <h3>
+                  PhaserJS</h3>
+                               
+                   </v-flex>
+      
+                                 </v-layout>
                 </v-flex>
               </v-layout>
             </v-card>
@@ -286,8 +360,7 @@
                   </v-card-title>
                   <v-divider></v-divider>
                   <v-card-text>
-                    In dit project heb ik als eerste een API gemaakt met de node, express en mongoDB. Daarna heb ik een Front-End gemaakt met Vuejs
-                    
+Dit is een kleine game die ik heb gemaakt met Typescript. In de game bestuur jij een panda en het is de bedoeling dat je alle donuts opvangt. Elke donut is een punt, raakt de donut de onderkant dan ben je af. Daarnaast slaat die ook nog de Highscore op. Dit was de eerste keer dat ik een game heb geprogrammeerd en ook de eerste keer werken met Typescript.                    
 
                   </v-card-text>
                 
@@ -302,15 +375,24 @@
                   </v-card-text>
                 </v-flex>
                 <v-flex xs2>
-                  <v-card-text>
-                    <v-icon large color="red">fa-youtube</v-icon>
+                  <v-card-text> <a href="http://zoehey.nl/game/docs/">
+                    <v-icon large color="red">fa-mouse-pointer</v-icon> </a>
                   </v-card-text>
                 </v-flex>
                 <v-flex xs1>
                   <v-divider vertical></v-divider>
                 </v-flex>
-                <v-flex xs7>
-                  lang
+                                                 <v-flex xs7 align-content-space-around>
+                                 <v-layout row>
+
+                  <v-flex xs4>
+               
+                   <h3>
+                  Typescript</h3>
+                               
+                   </v-flex>
+      
+                                 </v-layout>
                 </v-flex>
               </v-layout>
             </v-card>
